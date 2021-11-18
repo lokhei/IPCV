d00c56299eb0a8994c6a1066cc4461d041df16c7
lokhei <lokhei01@gmail.com>
2021-11-18 19:34:24 +0000

    hough transform

--- lab4/sobel.cpp
+++ lab4/sobel.cpp
@@ -9,6 +9,12 @@ using namespace cv;
 
 void sobel(cv::Mat &input);
 void normalise(Mat &input, string name);
+void GaussianBlur(
+	cv::Mat &input, 
+	int size,
+	cv::Mat &blurredOutput);
+
+
 
 int main( int argc, char** argv ) {
 
@@ -26,6 +32,8 @@ int main( int argc, char** argv ) {
 	// CONVERT COLOUR, BLUR AND SAVE
 	Mat gray_image;
 	cvtColor( image, gray_image, CV_BGR2GRAY );
+	// Mat blurred;
+    // GaussianBlur(gray_image,8,blurred);
 
 	sobel(gray_image);
 
@@ -118,5 +126,62 @@ void normalise(Mat &input, string name) {
 		}
 	}
 	imwrite( "coin_" + name + ".jpg", input );
+}
+
+
+
+void GaussianBlur(cv::Mat &input, int size, cv::Mat &blurredOutput)
+{
+	// intialise the output using the input
+	blurredOutput.create(input.size(), input.type());
+
+	// create the Gaussian kernel in 1D 
+	cv::Mat kX = cv::getGaussianKernel(size, -1);
+	cv::Mat kY = cv::getGaussianKernel(size, -1);
+	
+	// make it 2D multiply one by the transpose of the other
+	cv::Mat kernel = kX * kY.t();
+
+	//CREATING A DIFFERENT IMAGE kernel WILL BE NEEDED
+	//TO PERFORM OPERATIONS OTHER THAN GUASSIAN BLUR!!!
+
+	// we need to create a padded version of the input
+	// or there will be border effects
+	int kernelRadiusX = ( kernel.size[0] - 1 ) / 2;
+	int kernelRadiusY = ( kernel.size[1] - 1 ) / 2;
+
+	cv::Mat paddedInput;
+	cv::copyMakeBorder( input, paddedInput, 
+		kernelRadiusX, kernelRadiusX, kernelRadiusY, kernelRadiusY,
+		cv::BORDER_REPLICATE );
+
+	// now we can do the convoltion
+	for ( int i = 0; i < input.rows; i++ )
+	{	
+		for( int j = 0; j < input.cols; j++ )
+		{
+			double sum = 0.0;
+			for( int m = -kernelRadiusX; m <= kernelRadiusX; m++ )
+			{
+				for( int n = -kernelRadiusY; n <= kernelRadiusY; n++ )
+				{
+					// find the correct indices we are using
+					int imagex = i + m + kernelRadiusX;
+					int imagey = j + n + kernelRadiusY;
+					int kernelx = m + kernelRadiusX;
+					int kernely = n + kernelRadiusY;
+
+					// get the values from the padded image and the kernel
+					int imageval = ( int ) paddedInput.at<uchar>( imagex, imagey );
+					double kernalval = kernel.at<double>( kernelx, kernely );
+
+					// do the multiplication
+					sum += imageval * kernalval;							
+				}
+			}
+			// set the output value as the sum of the convolution
+			blurredOutput.at<uchar>(i, j) = (uchar) sum;
+		}
+	}
 }
 
