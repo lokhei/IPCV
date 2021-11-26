8e7c9dc91d09bf6348126213c8157d060c813e00
lokhei <lokhei01@gmail.com>
2021-11-26 16:57:44 +0000

    Task 1a - Ground Truth

--- CW/face.cpp
+++ CW/face.cpp
@@ -13,12 +13,15 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 #include <stdio.h>
+#include <fstream>
 
 using namespace std;
 using namespace cv;
 
 /** Function Headers */
 void detectAndDisplay( Mat frame );
+void read_csv(string name, vector<Rect> &groundTruths, Mat frame );
+
 
 /** Global variables */
 String cascade_name = "frontalface.xml";
@@ -33,15 +36,74 @@ int main( int argc, const char** argv )
 
 	// 2. Load the Strong Classifier in a structure called `Cascade'
 	if( !cascade.load( cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+	
+	
+	vector<Rect> groundTruths;
+	read_csv(argv[1], groundTruths, frame);
 
 	// 3. Detect Faces and Display Result
 	detectAndDisplay( frame );
+
+	
 
 	// 4. Save Result Image
 	imwrite( "detected.jpg", frame );
 
 	return 0;
 }
+vector<string> splitString(string s, string delimiter){
+	vector<string> tokens;
+
+	int start = 0;
+	int end = s.find(delimiter);
+	while (end != -1){
+		tokens.push_back(s.substr(start,end-start));
+		start = end + delimiter.size();
+		end = s.find(delimiter, start);
+	}
+	tokens.push_back(s.substr(start,end-start));
+/*
+	int position; 
+
+
+	while((position = line.find(delimiter)) != string::npos){
+		tokens.push_back(line.substr(0, position));
+		line.erase(0, position+1);
+	}
+	tokens.push_back(line);
+*/
+	return tokens;
+	
+
+
+}
+void read_csv(string filePath, vector<Rect> &groundTruths, Mat frame ){
+
+	ifstream file("labels.csv");
+	string line;
+
+	vector<string> fileName = splitString(filePath, "/");
+
+	string imageName = splitString(fileName[fileName.size()-1], ".")[0];
+
+
+
+	while(getline(file, line)){
+
+		vector<string> col = splitString(line, ",");
+		if (splitString(col[0], ".")[0] == imageName) {
+			groundTruths.push_back(Rect(atoi(col[1].c_str()), atoi(col[2].c_str()), atoi(col[3].c_str()), atoi(col[4].c_str())));
+		}
+	}
+
+	for (int i = 0; i < groundTruths.size(); i++){
+		rectangle(frame, Point(groundTruths[i].x, groundTruths[i].y), Point(groundTruths[i].x + groundTruths[i].width, groundTruths[i].y + groundTruths[i].height), Scalar( 0, 0, 255 ), 2);
+	}
+	
+	file.close();
+}
+
+
 
 /** @function detectAndDisplay */
 void detectAndDisplay( Mat frame )
