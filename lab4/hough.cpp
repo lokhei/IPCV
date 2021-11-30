7866a4ddf63d2a3eddced360527fe2f6a4ebfe1f
lokhei <lokhei01@gmail.com>
2021-11-30 18:24:36 +0000

    Lab 4 hough

--- lab4/hough.cpp
+++ lab4/hough.cpp
@@ -10,7 +10,8 @@ using namespace cv;
 void sobel(cv::Mat &input, cv::Mat &deriv_x, cv::Mat &deriv_y,  cv::Mat &direction,  cv::Mat &magnitude);
 void GaussianBlur(cv::Mat &input, int size,cv::Mat &blurredOutput);
 void threshold(Mat &input, int threshold, Mat &output);
-void hough_transform(Mat &input, Mat &mag, Mat &direction, int r_min, int r_max, double threshold);
+vector<vector<int> > hough_transform(Mat &input, Mat &mag, Mat &direction, int r_min, int r_max, double threshold);
+void drawCircles(Mat &input, vector<vector<int> > circles);
 
 int ***malloc3dArray(int dim1, int dim2, int dim3) {
     int i, j, k;
@@ -78,7 +79,11 @@ int main( int argc, char** argv ) {
     Mat gray_thres;
     cvtColor( thres_mag, gray_thres, CV_BGR2GRAY );
     threshold(gray_thres, 50, thres_mag);
-    hough_transform(image, thres_mag, direction, 10, 100, 20);
+    // vector<vector<int> > circles = hough_transform(image, thres_mag, direction, 10, 100, 20);
+    vector<vector<int> > circles = hough_transform(image, thres_mag, direction, 5, 70, 14);
+
+    drawCircles(image, circles);
+
 
     return 0;
 }
@@ -127,7 +132,7 @@ void sobel(cv::Mat &input, cv::Mat &deriv_x, cv::Mat &deriv_y, cv::Mat &directio
     }
 }
 
-void hough_transform(Mat &image, Mat &mag,  Mat &direction, int r_min, int r_max, double threshold) {
+vector<vector<int> > hough_transform(Mat &image, Mat &mag,  Mat &direction, int r_min, int r_max, double threshold) {
 
     int ***hough_space = malloc3dArray(mag.rows, mag.cols, r_max);
     for (int i = 0; i < mag.rows; i++) {
@@ -167,12 +172,8 @@ void hough_transform(Mat &image, Mat &mag,  Mat &direction, int r_min, int r_max
     //sum  values of the radius dimension
     for (int x = 0; x < mag.rows; x++) {
         for (int y = 0; y < mag.cols; y++) {
-            bool flag = false;
             for (int r = r_min; r < r_max; r++) {
                 hough_output.at<float>(x,y) += hough_space[x][y][r];
-                if (hough_space[x][y][r]>threshold){
-                    circle(image, Point(y,x), r, (0,0,255),1,8,0);
-                }
             }
         }
     }
@@ -181,10 +182,66 @@ void hough_transform(Mat &image, Mat &mag,  Mat &direction, int r_min, int r_max
     Mat hough_norm(mag.rows, mag.cols, CV_8UC1);
     normalize(hough_output, hough_norm, 0, 255, NORM_MINMAX);
     imwrite( "hough.jpg", hough_norm );
+
+
+
+    vector<vector<int> > circles;
+	for (int x = 0; x < image.rows; x++) {
+        for (int y = 0; y < image.cols; y++) {
+			bool test_pass = true;
+
+            int currentMax = 0;
+            int maxRadius = 0;
+
+            // find max radius
+            for (int r = r_min; r < r_max; r++) {
+				if(hough_space[x][y][r] > currentMax) {
+					currentMax = hough_space[x][y][r];
+                    maxRadius = r;
+				}
+            }
+			
+			for(int i = 0; i < circles.size(); i++) {
+				vector<int> circle = circles[i];
+				int xc = circle[0];
+				int yc = circle[1];
+				int rc = circle[2];
+
+				if(!(pow((x-xc),2) + pow((y-yc),2) > pow(rc,2))) {
+					test_pass = false;
+				}
+			}
+			if(hough_space[x][y][maxRadius] > threshold && test_pass) {
+				vector<int> circle;
+				circle.push_back(x);
+				circle.push_back(y);
+				circle.push_back(maxRadius);
+				circles.push_back(circle);
+			}
+        }
+    }
+
+	std::cout << "circles: " << circles.size() << std::endl;
+
+
     imwrite( "detectedCircles.jpg", image );
+
+    return circles;
 }
 
+void drawCircles(Mat &input, vector<vector<int> > circles) {
 
+	for(int i = 0; i < circles.size(); i++) {
+		vector<int> c = circles[i];
+		Point center = Point(c[1], c[0]);
+		circle(input, center, 1, Scalar(0, 255, 0), 3, 8, 0);
+		int radius = c[2];
+		circle(input, center, radius, Scalar(0, 0, 255), 2, 8, 0);
+	}
+
+	imwrite("detectedCircles.jpg", input);
+
+}
 
 
 
